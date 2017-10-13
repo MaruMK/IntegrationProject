c84a99c58387fc4f6029d74dd3f4d4a2c3656823
Mamoru-Maru Kajifusa <1411266@johnabbottcollege.net>
2017-10-13 12:56:23 -0400

    Mor testing

---
+++ IntegrationProjectNMGM/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//TESTING CHANGES
+namespace IntegrationProjectNMGM.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            int test = 1;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
