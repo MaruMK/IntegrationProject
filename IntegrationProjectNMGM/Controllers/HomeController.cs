dea27ba2e3d5224e2473adc58b0518959e55792e
Nathan Jasiukajc <1068028@johnabbottcollege.net>
2017-11-03 14:05:47 -0400

    setting up store

--- IntegrationProjectNMGM/Controllers/HomeController.cs
+++ IntegrationProjectNMGM/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace IntegrationProjectNMGM.Controllers
             return View();
         }
 
+        public ActionResult Store()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
