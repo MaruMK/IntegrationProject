8801985ee6f1b97d3998cbd5b2ad64514cda3ba5
Gabriel Charlebois <1536401@johnabbottcollege.net>
2017-11-10 12:27:54 -0500

    Product controller changed to return Product, Reviews and Images into the view.

--- IntegrationProjectNMGM/Models/ProductDetails.cs
+++ IntegrationProjectNMGM/Models/ProductDetails.cs
@@ -6,11 +6,12 @@ using System.Web;
 namespace IntegrationProjectNMGM.Models
 {
     //CREATED BY GABRIEL CHARLEBOIS
-    //This class is used to store a product its Reviews.
+    //This class is used to store a product, its Reviews and its Images.
     //It is used to pass from the Products controller into the Details view
-    public class ProductAndReviews
+    public class ProductDetails
     {
         public IEnumerable<Review> Reviews { get; set; }
+        public IEnumerable<Image> Images { get; set; }
         public Product CurrentProduct { get; set; }
     }
 }
